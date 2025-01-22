c6ef0c7b9819051aa90ec1d7e56f6912d490db69
PolRobledillo <pol.robledillo.7e7@itb.cat>
2025-01-22 15:52:33 +0100

    started implementing hurt state

--- BrawlToons/Assets/Scripts/Player/Character.cs
+++ BrawlToons/Assets/Scripts/Player/Character.cs
@@ -15,11 +15,22 @@ public class Character : MonoBehaviour, IDamageable
     {
         health -= damage;
         CameraShaker.Instance.ShakeOnce(2f, 3f, 0.1f, 0.5f);
+        try
+        {
+            Player1Behaviour player1Behaviour = GetComponent<Player1Behaviour>();
+            player1Behaviour.currentState = Player1Behaviour.Player1State.Hurt;
+            Debug.Log("Player 1 Hit");
+        }
+        catch
+        {
+            Player2Behaviour player2Behaviour = GetComponent<Player2Behaviour>();
+            player2Behaviour.currentState = Player2Behaviour.Player2State.Hurt;
+            Debug.Log("Player 2 Hit");
+        }
 
         if (health <= 0)
         {
-
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
 
     }
