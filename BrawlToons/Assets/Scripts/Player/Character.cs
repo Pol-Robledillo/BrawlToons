4333935514dc04c068f51f79ed8b982e17d9f93d
marta [itb] alarcón valdés <marta.alarcon.7e7@itb.cat>
2025-01-22 18:23:33 +0100

    hurt animation and coroutine

--- BrawlToons/Assets/Scripts/Player/Character.cs
+++ BrawlToons/Assets/Scripts/Player/Character.cs
@@ -19,13 +19,11 @@ public class Character : MonoBehaviour, IDamageable
         {
             Player1Behaviour player1Behaviour = GetComponent<Player1Behaviour>();
             player1Behaviour.currentState = Player1Behaviour.Player1State.Hurt;
-            Debug.Log("Player 1 Hit");
         }
         catch
         {
             Player2Behaviour player2Behaviour = GetComponent<Player2Behaviour>();
             player2Behaviour.currentState = Player2Behaviour.Player2State.Hurt;
-            Debug.Log("Player 2 Hit");
         }
 
         if (health <= 0)
--- BrawlToons/Assets/Scripts/Player/Player2Behaviour1.cs
+++ BrawlToons/Assets/Scripts/Player/Player2Behaviour1.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static Player1Behaviour;
+
 public class Player2Behaviour : MonoBehaviour
 {
     public float moveSpeed = 5f;
@@ -16,6 +18,16 @@ public class Player2Behaviour : MonoBehaviour
     }
     public Player2State currentState = Player2State.Idle;
 
+    public float knockbackForce = 0.3f;
+    public float knockbackDuration = 0.1f;
+    private Vector2 knockbackDirection;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
     void Update()
     {
         switch (currentState)
@@ -29,6 +41,7 @@ public class Player2Behaviour : MonoBehaviour
             case Player2State.Jumping:
                 break;
             case Player2State.Hurt:
+                Hurt();
                 break;
             case Player2State.Attacking:
                 break;
@@ -44,7 +57,36 @@ public class Player2Behaviour : MonoBehaviour
         moveInput = Vector2.zero;
         
     }
-   
+    public void Hurt()
+    {
+        animator.SetBool("hurt", true);
+        // Establecer dirección del knockback (puedes modificar esta lógica dependiendo de cómo se recibe el golpe)
+        knockbackDirection = new Vector2(1, 0); // Retroceder hacia la izquierda (modificar según la situación)
+        // Iniciar la corrutina de knockback
+        StartCoroutine(DoKnockback());
+    }
+
+    // Corrutina que maneja el knockback
+    private IEnumerator DoKnockback()
+    {
+        // Cambiar el estado a Hurt (esto podría ser modificado si es necesario)
+        currentState = Player2State.Hurt;
+
+        float timer = 0f;
+
+        // Mover al jugador en la dirección del knockback durante el tiempo definido
+        while (timer < knockbackDuration)
+        {
+            transform.Translate(knockbackDirection * knockbackForce * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null; // Esperar un frame
+        }
+
+        // Una vez terminado el knockback, cambiar al estado Idle
+        animator.SetBool("hurt", false);
+        currentState = Player2State.Idle;
+    }
+
 }
  
 
