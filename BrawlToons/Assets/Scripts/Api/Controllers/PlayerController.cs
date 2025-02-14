5e199f306e987a690c92e0aa537eb3d7b2f7660b
RaulPalomo <raul.palomo.7e7@itb.cat>
2025-02-14 16:52:03 +0100

    scripts api y newtonsoft

---
+++ BrawlToons/Assets/Scripts/Api/Controllers/PlayerController.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+//referencia a los modelos
+using BrawlToonsAPI.Models;
+
+public class PlayerController : MonoBehaviour
+{
+    //url básica de la api
+    private const string ApiBaseUrl = "http://localhost:5000/api/player";
+
+    //funciones para llamar a las corrutinas
+    public void GetPlayerFunc(int id)
+    {
+        StartCoroutine(GetPlayer(id));
+        
+    }
+
+    public void PostPlayerFunc(BrawlToonsAPI.Models.Player player)
+    {
+        
+        StartCoroutine(AddPlayer(player));
+
+    }
+
+    public void UpdatePlayerFunc(BrawlToonsAPI.Models.Player player)
+    {
+        StartCoroutine(UpdatePlayer(player));
+    }
+
+    public void VerifyUserFunc(string username, string passwd)
+    {
+        StartCoroutine(VerifyUser(username, passwd));
+    }
+    // Implementa endpoint GET: api/player/GET/{id}
+    public IEnumerator GetPlayer(int id)
+    {
+        Debug.Log("aaa");
+        using (UnityWebRequest request = UnityWebRequest.Get($"{ApiBaseUrl}/GET/{id}"))
+        {
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                BrawlToonsAPI.Models.Player player = JsonConvert.DeserializeObject<BrawlToonsAPI.Models.Player>(request.downloadHandler.text);
+                Debug.Log($"Id: {player.player_id}");
+                Debug.Log($"Games played: {player.games_played}");
+            }
+            else
+            {
+                Debug.LogError($"Error: {request.error}");
+            }
+        }
+    }
+
+    // Implementa endpoint POST: api/player
+    public IEnumerator AddPlayer(BrawlToonsAPI.Models.Player player)
+    {
+        string jsonData = JsonConvert.SerializeObject(player);
+        using (UnityWebRequest request = new UnityWebRequest(ApiBaseUrl, "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                BrawlToonsAPI.Models.Player createdPlayer = JsonConvert.DeserializeObject<BrawlToonsAPI.Models.Player>(request.downloadHandler.text);
+                Debug.Log(createdPlayer);
+            }
+            else
+            {
+                Debug.LogError($"Error: {request.error}");
+            }
+        }
+    }
+
+    // Implementa endpoint GET: api/player/GET/{username},{password}
+    public IEnumerator VerifyUser(string username, string password)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get($"{ApiBaseUrl}/GET/{username},{password}"))
+        {
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                BrawlToonsAPI.Models.Player player = JsonConvert.DeserializeObject<BrawlToonsAPI.Models.Player>(request.downloadHandler.text);
+                Debug.Log("Todo Bien");
+            }
+            else
+            {
+                Debug.LogError($"Error: {request.error}");
+            }
+        }
+    }
+
+    // Implementa endpoint PUT: api/player/UPDATE
+    public IEnumerator UpdatePlayer(BrawlToonsAPI.Models.Player player)
+    {
+        string jsonData = JsonConvert.SerializeObject(player);
+        using (UnityWebRequest request = new UnityWebRequest($"{ApiBaseUrl}/UPDATE", "PUT"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"Error: {request.error}");
+            }
+        }
+    }
+}
---
+++ BrawlToons/Assets/Scripts/Api/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace BrawlToonsAPI.Models
+{
+    public class Player
+    {
+        public int player_id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int games_played { get; set; } = 0;
+        public int total_wins { get; set; } = 0;
+        public int total_losses { get; set; } = 0;
+
+        public Player(string username,string pasword) {
+            this.username = username; 
+            this.password = pasword; 
+            this.games_played = 0;
+            this.total_wins = 0;
+            this.total_losses = 0;
+        }
+    }
+}
